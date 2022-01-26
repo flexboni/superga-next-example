1c7423c8a778b51b6b74f5af78e7d4154cd6c75e
flexboni <kgb8594@gmail.com>
2022-01-26 16:29:35 +0900

    git rebase test2

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Head from "next/head";
 import Layout, { siteTitle } from "../components/layout";
 import { getSortedPostsData } from "../lib/posts";
 import Link from "next/link";
-import Date from "../components/date";
+import Date from "src/components/date";
 
 export async function getStaticProps() {
   const allPostsData = getSortedPostsData();
@@ -21,10 +21,10 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section>
-        <p>I love React. I love Next.js</p>
+        <p>I love React. I love Next.js!</p>
         <p>
           This is a sample website - you will be building a site like this on
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>
+          <a href="https://nextjs.org/learn">our Next.js tutorial!</a>
         </p>
       </section>
 
--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -7,13 +7,13 @@ const FirstPost = () => {
   return (
     <Layout>
       <Head>
-        <title>First Post</title>
+        <title>First Post!</title>
       </Head>
-      <h1>First Post!!</h1>
+      <h1>First Post!!!!!</h1>
 
       <h2>
         <Link href="/">
-          <a>Back to home</a>
+          <a>Back to home!!!</a>
         </Link>
       </h2>
     </Layout>
